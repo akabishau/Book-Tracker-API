1efea7c5fde7477f48bacd66f212ebe13989ba5e
akabishau <aleksey.kabishau@icloud.com>
2024-01-19 21:52:36 -0500

    complete basis user auth using devise-token-auth

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,16 @@
 class StatusController < ApplicationController
+  before_action :authenticate_user!, only: [:auth_check]
+
   def check
-    render json: { status: "Service is up" }
+    render json: { status: "Service is up" }, status: :ok
+  end
+
+  # status is http status code
+  def auth_check
+    if user_signed_in?
+      render json: { authenticated: true, user: current_user }, status: :ok
+    else
+      render json: { authenticated: false }, status: :unauthorized
+    end
   end
 end
