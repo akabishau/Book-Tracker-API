1aa2d686cded72686017fa4903deb528b0ac256e
akabishau <aleksey.kabishau@icloud.com>
2024-02-01 22:05:29 -0500

    apply fix for DisabledSessionError (Devise trying to use session for api-only app)
    solution for internet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # allows token generation, validation and rotation
+  # add Devise modules to the User model
   include DeviseTokenAuth::Concerns::User
 
   has_many :user_books
--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,5 @@ module BookTrackerApi
   class Application < Rails::Application
     config.load_defaults 7.0
     config.api_only = true
-
-    # # enabling for now but using tokens instead
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
